76ed3e42a97ef3d0d8299997551b2251517e9a6b
stephHiyagon <mv.hiyagon@gmail.com>
2017-03-03 18:25:46 -0500

    ejercicio de etiqueta enlace

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var enlaces = document.getElementsByTagName('a');
+console.log(enlaces);
+var result= enlaces.length;
+console.log(result);
+var cont=0;
+for(var i=0; i<result; i++){
+  if(enlaces[i].href==""){
+    cont += 0;
+  }else{
+    cont = 1;
+  }
+}
+document.getElementById('num_enlaces').innerHTML = cont;
